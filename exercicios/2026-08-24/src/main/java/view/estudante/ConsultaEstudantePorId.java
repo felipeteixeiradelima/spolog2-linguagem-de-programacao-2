78919aecf0fd7b780444ba4f015377f0af75269d
Felipe Teixeira de Lima <felipe.teixeiralima2511@gmail.com>
2026-08-26 11:16:19 -0300

    Arruma tamanho do nome na exibição

--- exercicios/2026-08-24/src/main/java/view/estudante/ConsultaEstudantePorId.java
+++ exercicios/2026-08-24/src/main/java/view/estudante/ConsultaEstudantePorId.java
@@ -9,7 +9,7 @@ public class ConsultaEstudantePorId {
 
     public static void exibeInterface() {
         Long id;
-        String formato = "%1$-3s %2$-30s %3$-6s %4$-6s %5$-6s%n";
+        String formato = "%1$-3s %2$-50s %3$-6s %4$-6s %5$-6s%n";
 
         System.out.println("\nCONSULTA DE ESTUDANTE POR ID:");
         id = ScannerUtil.extractId();
--- exercicios/2026-08-24/src/main/java/view/estudante/ConsultaEstudantes.java
+++ exercicios/2026-08-24/src/main/java/view/estudante/ConsultaEstudantes.java
@@ -42,7 +42,7 @@ public class ConsultaEstudantes {
     }
 
     public static void exibeInterface() {
-        String formato = "%1$-3s %2$-30s %3$-6s %4$-6s %5$-6s%n";
+        String formato = "%1$-3s %2$-50s %3$-6s %4$-6s %5$-6s%n";
 
         System.out.println("\nCONSULTA DE ESTUDANTES:");
         System.out.printf(formato, "ID", " | NOME", " | SEXO", " | PDC", " | IRA");
